49529bf701801f3e90686afacc62b10f1710abd8
WASHIUL ISLAM <washiul92@gmail.com>
2022-10-10 20:08:07 +0530

    added condition&expression

---
+++ Expression&Operators.js
@@ -0,0 +1,81 @@
+console.log("arithmatic Operators is JS")
+
+let a=10, b=4;
+
+
+
+console.log("a+b", a+b);
+
+console.log("a-b", a-b);
+
+console.log("a/b", a/b);
+
+console.log("a%b", a%b);
+
+console.log("a*b", a*b);
+
+console.log("a**b", a**b ); // a^b a power b
+
+console.log("a++", a++);
+
+console.log("++a", ++a);
+
+console.log("a--", a--);
+
+console.log("--a", --a);
+
+console.log("a", a);
+
+console.log("a--", a--);
+
+console.log("a", a);
+
+
+
+//Asignment operator 
+
+let Assignment = 1;
+
+Assignment+=5
+
+console.log(Assignment );
+
+Assignment-=5
+
+console.log( Assignment);
+
+Assignment/=5
+
+console.log( Assignment);
+
+Assignment*=5
+
+console.log( Assignment);
+
+Assignment**=5
+
+console.log( Assignment);
+
+//Comporasion Operators
+
+comp1=5;
+comp2=9;
+
+console.log("comp1=comp2 is",comp1==comp2);
+console.log("comp1 not equal to comp", comp1!=comp2);
+
+let com1=5;
+let com2="5";
+
+console.log(com1===com2); // === identifies the difference between a string and a number whereas == doesn't
+console.log(com1!==com2); 
+
+
+//Logical operator
+
+let lo1=4;  lo2=5;
+
+console.log(lo1<lo2 && lo1==4);
+console.log(lo1>lo2 && lo1==4);
+console.log(lo1<lo2 || lo1!=4);
+
